4444c844ebc059011107938eb7bae2c8cfff9919
T-Stolf <thomsstolf@gmail.com>
2021-07-25 11:41:01 -0400

    Fixed self-created error due to copypaste

--- app.js
+++ app.js
@@ -1,18 +1,59 @@
 'use strict'
-const phone = document.getElementById("phone");
-phone.addEventListener('click', function()
-{
-    alert("416-434-6088");
-});
+// const phone = document.getElementById("phone");
+// phone.addEventListener('click', function()
+// {
+//     alert("416-434-6088");
+// });
 
-const mail = document.getElementById("email");
-mail.addEventListener('click', function()
-{
-    alert("thomsstolf@gmail.com");
-});
+// const mail = document.getElementById("email");
+// mail.addEventListener('click', function()
+// {
+//     alert("thomsstolf@gmail.com");
+// });
 
-const disc = document.getElementById("discord");
-disc.addEventListener('click', function()
+// const disc = document.getElementById("discord");
+// disc.addEventListener('click', function()
+// {
+//     alert("Kasfas#1675");
+// });
+
+function aboutme()
 {
-    alert("Kasfas#1675");
-});
+    window.open("aboutme.html", "_self");
+}
+function projects()
+{
+    window.open("projects.html", "_self");
+}
+function experience()
+{
+    window.open("experience.html", "_self");
+}
+function back()
+{
+    window.open("Index.html", "_self");
+}
+
+function contact(type)
+{
+    switch(type)
+    {
+        case 0:
+            window.alert("416-434-6088");
+            break;
+        case 2:
+            window.alert("Kasfas#1675");
+            break;
+        case 1:
+            window.alert("thomsstolf@gmail.com");
+            break;
+        default:
+            window.alert("error");
+            break;
+    }
+
+}
+function gitHub()
+{
+    window.open("https://github.com/T-Stolf");
+}
